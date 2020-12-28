83984a3d66d44e83e123050d8fb8a3ea89560188
Starcatch <timurmishiev@gmail.com>
2020-12-28 16:04:24 -0500

    first commit

---
+++ src/sample-restaurants.js
@@ -0,0 +1,21 @@
+// burger hubs
+const restaurants = [
+  {
+    title: 'Burger Hub Cortelyou road',
+    url: 'hot-burger-cortelyou',
+    id: 1
+  },
+
+  {
+    title: 'Burger Hub Маросейка',
+    url: 'hot-burger-maroseyka',
+    id: 2
+  },
+  {
+    title: 'Burger Hub Петровка',
+    url: 'hot-burger-petrovka',
+    id: 3
+  }
+];
+
+export default restaurants;
