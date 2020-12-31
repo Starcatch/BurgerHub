2de81309d799be34afc2f26c99be13d8b2999da0
Starcatch <timurmishiev@gmail.com>
2020-12-31 18:50:00 -0500

    build Order component

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -16,7 +16,7 @@ class Burger extends React.Component {
       <div className='burger-details'>
         <h3 className='burger-name'>
           {name},
-          <span  className='price'>{price}</span>
+          <span  className='price'> $ {price}</span>
         </h3>
         <p>{desc}></p>
         <button 
--- src/components/Order.js
+++ src/components/Order.js
@@ -2,21 +2,59 @@ import React from 'react';
 
 
 class Order extends React.Component {
+
+  renderOrder = (key) => {
+
+    const burger = this.props.burgers[key];
+    const count = this.props.order[key];
+    const isAvailable = burger && burger.status === 'available';
+    if (!isAvailable) {
+      return <li className='unavailable'>
+        Sorry, {burger ? burger.name : 'burger'} temrerary sold out
+      </li>
+    }
+    return (
+      <li>
+      <span>
+        <span>{count}</span>
+        items: {burger.name}
+        <span> $ {count * burger.price} </span>
+        <button className='cancelItem'>&times;</button>
+      </span>
+    </li>
+    );
+     
+  };
+
   render() {
     const orderIds = Order.keys(this.props.order);
     const total = orderIds.reduce((prevTotal, key) =>{
       const burger = this.props.burgers[key];
       const count = this.props.order[key];
 
-    }, 0)
+      const isAvailable = burger && burger.status === 'available';
+      if (isAvailable) {
+        // if our burger is available then we show it in total price of the order
+          return prevTotal + burger.price * count;
+      }
+      // if the burger is not available we just return a price with out adding the burger to our total price.
+      return prevTotal 
+    }, 0);
 
     return(
       <div className='order-wrap'>
         <h2>Your Order</h2>
-        <ul className='order'>{orderIds.map(key =>  {
-          return <li>{key}</li>
+        <ul className='order'>
+          {orderIds.map(this.renderOrder)}
+        </ul>
+        <div className='total'>
+          <div className='total_wrap'>
+            <div className='total_wrap-final'>
+              Total: {total}
+            </div>
 
-        })}</ul>
+          </div>
+        </div>
       </div>
     );
   }
