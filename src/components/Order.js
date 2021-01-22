d9ff402429334aefa173c44c58b11b164599a52f
Starcatch <timurmishiev@gmail.com>
2021-01-21 22:00:17 -0500

    utilize local storage

--- src/components/Order.js
+++ src/components/Order.js
@@ -9,6 +9,9 @@ class Order extends React.Component {
     const burger = this.props.burgers[key];
     const count = this.props.order[key];
     const isAvailable = burger && burger.status === 'available';
+    
+    if(!burger) return null;
+    
     if (!isAvailable) {
       return (
       <li className='unavailable' key={key}>
